0e5343af8b5571e3c7ca6665ffaad1857c5f4f26
whang <coolkilling@163.com>
2019-10-30 17:46:29 +0800

    20191030

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
 
 def index(request):
-    return HttpResponse("你好")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #template = loader.get_template('polls/index.html')
+    #context = {
+    #    'latest_question_list': latest_question_list,
+    #}
+    #return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("将为您打开问卷 %s。" % question_id)
+
+def results(request, question_id):
+    response = "正在查看问卷%s的结果。"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("请为问卷%s提交您的答案。" % question_id) 
